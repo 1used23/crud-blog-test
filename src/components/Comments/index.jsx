408c25f079f47ac9ced2d676be1305bb7a0028c6
1used23 <diziwii@gmail.com>
2020-03-15 03:38:18 +0300

    comments component

---
+++ src/components/Comments/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addNewComment } from "../../actions/commentsActions";
+import { Typography, TextField, Paper, Button } from "@material-ui/core";
+
+import "./Comments.scss";
+
+const Comments = ({ comments, id, addNewComment }) => {
+  const [commentValue, setCommentValue] = useState();
+
+  const newComment = () => {
+    if (!commentValue) {
+      return;
+    }
+    const commentData = {
+      postId: id,
+      id: (comments[0] && comments[comments.length - 1].id + 1) || 0,
+      body: commentValue
+    };
+    addNewComment(commentData);
+  };
+  return (
+    <div className="comments">
+      <Typography className="comments__title" variant="h4">
+        Comments
+      </Typography>
+
+      {comments.map(comment => {
+        return (
+          <Paper className="comments__el" key={comment.id}>
+            {comment.body}
+          </Paper>
+        );
+      })}
+      <div className="comments__input">
+        <TextField
+          multiline
+          value={commentValue}
+          onChange={e => {
+            setCommentValue(e.currentTarget.value);
+          }}
+        />
+        <Button
+          onClick={() => {
+            newComment();
+            setCommentValue("");
+          }}
+        >
+          Add
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  addNewComment: ({ postId, id, body }) =>
+    dispatch(addNewComment({ postId, id, body }))
+});
+
+export default connect(null, mapDispatchToProps)(Comments);
--- src/components/PostPage/index.jsx
+++ src/components/PostPage/index.jsx
@@ -1,43 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { closePost } from "../../actions/postsActions";
-import { addNewComment, getSelectedPost } from "../../actions/commentsActions";
+import { getSelectedPost } from "../../actions/commentsActions";
 import { Link, useParams } from "react-router-dom";
-import { Card } from "../../components";
-import {
-  IconButton,
-  Typography,
-  Paper,
-  TextField,
-  Button
-} from "@material-ui/core";
+import { Card, Comments } from "../../components";
+import { IconButton } from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 import "./PostPage.scss";
 
-const PostPage = ({
-  closePost,
-  addNewComment,
-  getSelectedPost,
-  selectedPost
-}) => {
+const PostPage = ({ closePost, getSelectedPost, selectedPost }) => {
   const { id } = useParams();
-  const [commentValue, setCommentValue] = useState();
-
-  const newComment = () => {
-    if (!commentValue) {
-      return;
-    }
-    const commentData = {
-      postId: id,
-      id:
-        (selectedPost.comments[0] &&
-          selectedPost.comments[selectedPost.comments.length - 1].id + 1) ||
-        0,
-      body: commentValue
-    };
-    addNewComment(commentData);
-  };
 
   useEffect(() => getSelectedPost({ id: id }), [id, getSelectedPost]);
 
@@ -57,36 +30,7 @@ const PostPage = ({
             </Link>
           </div>
           <Card post={selectedPost} single />
-          <div className="post-page__comments">
-            <Typography className="post-page__comments_title" variant="h4">
-              Comments
-            </Typography>
-
-            {selectedPost.comments.map(comment => {
-              return (
-                <Paper className="post-page__comments_el" key={comment.id}>
-                  {comment.body}
-                </Paper>
-              );
-            })}
-            <div className="post-page__comments_input">
-              <TextField
-                multiline
-                value={commentValue}
-                onChange={e => {
-                  setCommentValue(e.currentTarget.value);
-                }}
-              />
-              <Button
-                onClick={() => {
-                  newComment();
-                  setCommentValue("");
-                }}
-              >
-                Add
-              </Button>
-            </div>
-          </div>
+          <Comments comments={selectedPost.comments} id={id} />
         </>
       )}
     </div>
@@ -95,8 +39,6 @@ const PostPage = ({
 
 const mapDispatchToProps = dispatch => ({
   closePost: () => dispatch(closePost()),
-  addNewComment: ({ postId, id, body }) =>
-    dispatch(addNewComment({ postId, id, body })),
   getSelectedPost: ({ id }) => dispatch(getSelectedPost({ id }))
 });
 
